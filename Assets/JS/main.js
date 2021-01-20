4cc75ec32448cca8efb8722d6daa2b8fe44a17a7
ciscuokami <ciscumartinez@gmail.com>
2021-01-20 21:37:38 +0100

    primer intento

---
+++ Assets/JS/main.js
@@ -0,0 +1,142 @@
+const body = document.querySelector("body");
+
+
+
+const contenedor = document.createElement("div");
+contenedor.id = "contenedor";
+body.appendChild(contenedor);
+
+
+
+function paintWeatherData(name, weather, main) {
+    const body = document.querySelector("body");
+
+	const container = document.createElement("div");
+
+	const nameP = document.createElement("p");
+
+	nameP.innerText = name;
+
+    container.appendChild(nameP);
+
+    const mainP = document.createElement("p");
+
+	mainP.innerText = main.temp;
+
+	container.appendChild(mainP);
+
+	weather.map(weatherInfo => {
+		const weatherContainer = document.createElement("div");
+		const description = document.createElement("p");
+
+
+		description.innerText = weatherInfo.description;
+
+		weatherContainer.appendChild(description);
+
+		container.appendChild(weatherContainer);
+
+		return weatherContainer;
+    });
+
+	body.appendChild(container);
+}
+
+function paintWeather5diasData(city, list) {
+    const body = document.querySelector("body");
+
+	const container = document.createElement("div");
+
+	const nameP = document.createElement("p");
+
+	nameP.innerText = city.name;
+
+    container.appendChild(nameP);
+
+	list.map(weatherInfo => {
+		const weatherContainer = document.createElement("div");
+		const description = document.createElement("p");
+
+
+		description.innerText = weatherInfo.weather.description;
+
+		weatherContainer.appendChild(description);
+
+		container.appendChild(weatherContainer);
+
+		return weatherContainer;
+    });
+
+	body.appendChild(container);
+}
+
+function getWeatherData(inputVal) {
+    const apiKey = "e279c60dbd83ed07e28c53b28b7ff74e";
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metrics&lang=es`)
+	.then(response => response.json())
+	.then(data => {
+        console.log(data);
+		const {name, weather, main} = data;
+		paintWeatherData(name, weather, main);
+	});
+}
+
+function getWeather5diasData(inputVal) {
+    const apiKey = "e279c60dbd83ed07e28c53b28b7ff74e";
+	fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${apiKey}&units=metrics&lang=es`)
+	.then(response => response.json())
+	.then(data => {
+        console.log(data);
+		const {city, list} = data;
+		paintWeather5diasData(city, list);
+	});
+}
+
+
+function getCityName() {
+
+    const buscador = document.createElement("div");
+    const labelInput = document.createElement("label");
+    const inputCiudad = document.createElement("input");
+    const btn = document.createElement("button");
+    const btn5 = document.createElement("button");
+
+    buscador.id = "buscador";
+    labelInput.innerText = "Find City";
+    inputCiudad.placeholder = "Search City"
+    btn.innerText = "Search";
+    btn.preventDefault;
+    btn5.innerText = "5 Días";
+    btn5.preventDefault;
+
+    contenedor.appendChild(buscador);
+    buscador.appendChild(labelInput);
+    buscador.appendChild(inputCiudad);
+    buscador.appendChild(btn);
+    buscador.appendChild(btn5);
+
+
+    btn.addEventListener("click", () => {
+        const inputVal = inputCiudad.value;
+
+        if (inputVal) {
+            getWeatherData(inputVal);
+        }
+        else
+        console.log("No has introducido ninguna ciudad");
+        return inputVal;
+    });
+
+    btn5.addEventListener("click", () => {
+        const inputVal = inputCiudad.value;
+
+        if (inputVal) {
+            getWeather5diasData(inputVal);
+        }
+        else
+        console.log("No has introducido ninguna ciudad");
+        return inputVal;
+    });
+};
+
+getCityName();
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Aplicacion_tiempo
 Aplicacion del tiempo
+En el día de hoy vas a hacer una aplicación del tiempo, para ello vamos a hacer uso de la api https://openweathermap.org/ una vez más.
+
+Requisitos:
+
+La aplicación en primera instancia debe permitirte elegir una ubicación, en base a esta ubicación se mostrará el tiempo:
+
+Aparecerá el tiempo actual
+Aparecerá el tiempo de los próximos 7 días
+Por último, se deben mostrar las temperaturas de las próxima 24 horas
+
+
+Para añadir los iconos que te provee la api, puedes usar el siguiente enlace:
+
+`https://raw.githubusercontent.com/CodeExplainedRepo/Weather-App-JavaScript/master/icons/${iconCode}.png?raw=true`
+
+
+Pd: Es muy importante tu organización puesto que cuando termine la sesión de hoy se debe entregar la aplicación, se inteligente y prioriza funcionalidades.
